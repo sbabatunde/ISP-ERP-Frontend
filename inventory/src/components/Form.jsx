3a12b3b5ca31dd918ca0bcef133505b6c4a2d7ea
BakareHameed <bakarehameed07@gmail.com>
2025-01-28 18:21:36 -0800

    tailwind installation

--- inventory/src/components/Form.jsx
+++ inventory/src/components/Form.jsx
@@ -1,34 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import apiClient from '../api/axios';
+// import React, { useEffect, useState } from 'react';
+// import apiClient from '../api/axios';
+
+// const Form = () => {
+//     const [posts, setPosts] = useState([]);
+
+//     useEffect(() => {
+//         const fetchPosts = async () => {
+//             try {
+//                 const response = await apiClient.get('daily/generator/report/fuel-consumption'); 
+//                 // const response = fetch('http://localhost:8000/api/daily/generator/report/fuel-consumption'); 
+//                 console.log(response.data);
+//                 setPosts(response.data);
+//             } catch (error) {
+//                 console.error('Error fetching posts:', error);
+//             }
+//         };
+
+//         fetchPosts();
+//     }, []);
+
+//     return (
+//         <div>
+//             <h1 className='bg-blue-500 text-blue-600 font-bold underline'>Posts</h1>
+//             <ul>
+//                 {posts.map((post) => (
+//                     <li key={post.id}>{post.status}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
+
+// export default Form;
+
+
+import React from 'react'
 
 const Form = () => {
-    const [posts, setPosts] = useState([]);
+  return (
+    <div>
+        <h1  className='bg-blue-500 text-blue-600 font-bold underline'>Form testing</h1>
+    </div>
+  )
+}
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await apiClient.get('daily/generator/report/fuel-consumption'); 
-                // const response = fetch('http://localhost:8000/api/daily/generator/report/fuel-consumption'); 
-                console.log(response.data);
-                setPosts(response.data);
-            } catch (error) {
-                console.error('Error fetching posts:', error);
-            }
-        };
-
-        fetchPosts();
-    }, []);
-
-    return (
-        <div>
-            <h1>Posts</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>{post.status}</li>
-                ))}
-            </ul>
-        </div>
-    );
-};
-
-export default Form;
+export default Form
