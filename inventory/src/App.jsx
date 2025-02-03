c589c41ae8aa17e8df57ac463813ab1288e257a9
BakareHameed <bakarehameed07@gmail.com>
2025-02-03 01:12:22 -0800

    added equipment list

--- inventory/src/App.jsx
+++ inventory/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import Form from "./components/Form";
 import SuppliersForm from "./components/Suppliers";
 import EquipmentForm from "./pages/Equipment-procurement/EquipmentForm";
+import EquipmentList from "./pages/Equipment-procurement/EquipmentList";
 // import Sidebartest from "./components/Sidebartest";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="form" element={<Form />} />
           <Route path="suppliers" element={<SuppliersForm />} />
           <Route path="equipment-form" element={<EquipmentForm />} />
+          <Route path="equipment-list" element={<EquipmentList />} />
           {/* <Route path="about" element={<div>About Page</div>} />
           <Route path="contact" element={<div>Contact Page</div>} /> */}
         </Route>
