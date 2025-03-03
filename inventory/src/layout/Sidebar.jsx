e9f24d89a3812eb240659b2bf4640a8e48086e3c
Your Name <bakarehameed07@gmail.com>
2025-03-03 14:04:33 +0100

    new components

--- inventory/src/layout/Sidebar.jsx
+++ inventory/src/layout/Sidebar.jsx
@@ -1,7 +1,24 @@
-const Sidebar =() => {
-    return(
-        <aside>Sidebar</aside>
-    )
-}
+import {forwardRef} from "react"
+import {cn} from "../lib/cn"
+import { MdDashboard } from "react-icons/md";
+import { MdOutlineDashboard } from "react-icons/md";
+import PropTypes from "prop-types"
 
-export default Sidebar
+export const Sidebar = forwardRef(({collapsed}, ref) => {
+    return <aside ref={ref} 
+    className={cn(
+        "fixed z-[100] flex h-full w-[240px] flex-col overflow-x-hidden border-r border-slate-300 bg-white [transition:_width_300ms_cubic_bezier(0.4,_0,_0.2,_1),_left_300ms_cubic-bezier(0.4,_0,_0.2,_1),_background-color_150ms_cubic-bezier(0.4,_0,_0.2,_1)] dark:border-slate-700 dark:bg-slate-900")
+    }>
+        <div className="flex gap-x-3 p-3">
+            <MdDashboard className="dark:hidden" alt="logo"/>
+            <MdOutlineDashboard className="dark:block"/>
+            {!collapsed && <p className="text-lg font-medium text-slate-900 transition-colors dark:text-slate-50">Logo</p>}
+        </div>
+    </aside>
+})
+
+Sidebar.displayName = "Sidebar"
+
+Sidebar.PropTypes ={
+    collapsed: PropTypes.bool,
+}
--- inventory/src/routes/Layout.jsx
+++ inventory/src/routes/Layout.jsx
@@ -1,11 +1,25 @@
-import Sidebar from "../layout/Sidebar"
+import { Outlet } from "react-router-dom"
+
+import { useMediaQuery } from "@uidotdev/usehooks";
+
+import {Sidebar} from "../layout/Sidebar"
+import Header from "../layout/Header"
 import {cn} from "../lib/cn"
+import { useRef, useState } from "react";
 
 const Layout = () => {
+    // const isDesktopDevice = useMediaQuery("(min-width: 768px)");
+    // const [collapsed,setCollapsed] = useState(false)
+    // const sidebarRef = useRef(null)
     return(
         <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
             <Sidebar />
-            <div></div>
+            <div className={cn("transition-[margin] duration-300")}>
+                <Header/>
+                <div className="h-[calc(100vh-60px)] overflow-y-auto overflow-x-hidden p-6 ">
+                    <Outlet/>
+                </div>
+            </div>
         </div>
     )
 }
